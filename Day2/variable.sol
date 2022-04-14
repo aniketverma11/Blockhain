dabdd33bf8f5106211f0dd7038485e166b61396c
Aniket <aniketverma1103@gmail.com>
2022-04-14 13:43:29 +0530

    Variables Complete

---
+++ Day2/variable.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.8.13 ;
+
+contract Start {
+    // fixed size types
+    bool instanceof;
+    uint a;
+    uint b;
+    address my_address;
+    bytes32 Data;
+
+    //variable size types
+    string name;
+    bytes Bytes;
+    uint[] array;// initialize for array and contain only same datatype of elements into it
+    mapping(uint => string)  dictionary;
+
+
+    //user define data type
+
+    struct user {
+        uint id;
+        string name;
+        uint[] friendIds;
+    }
+    enum Color {
+        RED,
+        GREEN,
+        YELLOW,
+
+    }
+    Color.RED, Color.YELLOW 
+}
